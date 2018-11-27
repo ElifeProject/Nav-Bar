5b6699b1e0fe236e76299bae9661c52095835c50
AyeshaUmar <43723877+AyeshaUmar@users.noreply.github.com>
2018-11-27 20:45:23 +0500

    Add files via upload

---
+++ navigation_bar.php
@@ -0,0 +1,55 @@
+<!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
+
+                        <!-- Nav Brand -->
+                        <a href="index.php" class="nav-brand"><img src="img/logo.png" style="width: 100px;"></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+                            <div class="classynav font-cursive">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="#">Catalogue</a>
+                                        <ul class="dropdown">
+                                            <li><a href="E-Life_Computer_Accessories.php">Computer Peripherals</a></li>   
+                                            <li><a href="Project_Accessories.php">Project Accessories</a></li>
+                                            <li><a href="E-Life_Book_Store.php">Book Store</a></li>
+                                            <li><a href="Stationary_Store.php">Stationary Store</a></li>
+                                            <li><a href="E-Life_Grocery_Store.php">Grocery Store</a></li>           
+                                            <li><a href="Men's_Fashion.php">Men's Fashion</a></li>
+                                            <li><a href="Women's_Fashion.php">Women's Fashion</a></li> 
+                                            <li><a href="E-Life_Special.php">E-Life Special</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="SignUp.php">Sign Up</a></li>
+                                    <li><a href="login.php">Log In</a></li>
+                                    <li><a href="">Cart</a></li>
+                                </ul>
+                            </div>
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
